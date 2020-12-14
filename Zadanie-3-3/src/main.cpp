f8ff564a7bca4ee466cfc43d217ad35caff485fd
mngski <michal.niegowski@student.wat.edu.pl>
2020-12-14 10:38:33 +0100

    Wykonano zadanie 3.4.1 – PWM rand

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,37 +1,20 @@
-#define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f)
+#include <Arduino.h>
 
-uint16_t digital;
-float Voltage;
+#define PIN_G 5
+#define PIN_B 6
+#define PIN_R 9
 
-uint16_t ADCsingleREAD(uint8_t adcToUse){
-uint16_t ADCval;
+int r,g,b;
 
-ADMUX = adcToUse;
-ADMUX |= (1 << REFS0);
-ADMUX &= ~(1 << ADLAR);
-ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-ADCSRA |= (1 << ADEN);
-ADCSRA |= (1 << ADSC);
-while (ADCSRA & (1 << ADSC))
-  ;
-ADCval = ADCL;
-ADCval = (ADCH << 8 ) + ADCval;
-
-return ADCval; 
+void setup() {
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+  pinMode(PIN_R, OUTPUT);
 }
 
-void setup(){
-  Serial.begin(9600);
-}
-
-void loop(){
-  digital = ADCsingleREAD(0);
-  Voltage = ADC_RESOLUTION_DEFAULT * digital;
-  Serial.print("A0 = 102");
-  Serial.print(digital);
-  Serial.print(" \t V(0) = 0,5");
-  Serial.print(Voltage);
-  Serial.println(" [V]");
+void loop() {
+  analogWrite(PIN_R, rand());
+  analogWrite(PIN_G, rand());
+  analogWrite(PIN_B, rand());
   delay(1000);
 }
-    
